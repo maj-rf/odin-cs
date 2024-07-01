82b9df61c5b9cb565f6569479ec0a626f004608b
maj-rf <rmkfermin@gmail.com>
2024-07-01 18:31:06 +0800

    feat: implement at(), pop(), contains(), find(), and toString() methods

--- linked_list/LinkedList.ts
+++ linked_list/LinkedList.ts
@@ -28,6 +28,17 @@ export class LinkedList<T> {
     return tail;
   }
 
+  get size() {
+    if (!this._head) return 0;
+    let count = 1;
+    let next = this._head.next;
+    while (next !== null) {
+      count += 1;
+      next = next.next;
+    }
+    return count;
+  }
+
   append(value: T) {
     const node = new Node(value);
     if (!this._head) {
@@ -38,16 +49,65 @@ export class LinkedList<T> {
         tail.next = node;
       }
     }
-    return this;
   }
 
   prepend(value: T) {
-    const node = new Node(value);
-    if (!this.head) return null;
-    let prevHead = this.head;
-    node.next = prevHead;
-    this._head = node;
-    return this;
+    if (!this._head) return null;
+    const prevHead = this._head;
+    this._head = new Node(value, prevHead);
+  }
+
+  at(index: number) {
+    if (!this._head) return null;
+    if (index < 0) return null;
+    let pointer = this._head;
+    for (let i = 0; i < index; i++) {
+      if (pointer.next === null) return null;
+      pointer = pointer.next;
+    }
+    return pointer;
+  }
+
+  pop() {
+    if (!this._head) return null;
+    if (!this._head.next) {
+      this._head = null;
+      return;
+    }
+    let tail = this.at(this.size - 2);
+    if (!tail) return null;
+    tail.next = null;
+    return this._head;
+  }
+
+  contains(value: T) {
+    let pointer = this._head;
+    while (pointer !== null) {
+      if (pointer.value === value) return true;
+      pointer = pointer.next;
+    }
+    return false;
+  }
+
+  find(value: T) {
+    let pointer = this.head;
+    let index = 0;
+    while (pointer !== null) {
+      if (pointer.value === value) return index;
+      pointer = pointer.next;
+      index++;
+    }
+    return null;
+  }
+
+  toString() {
+    let pointer = this._head;
+    let string = '';
+    while (pointer !== null) {
+      string += `(${pointer.value}) -> `;
+      pointer = pointer.next;
+    }
+    return string + 'null';
   }
 }
 
@@ -55,6 +115,14 @@ const linkedList = new LinkedList(); // { head: null }
 linkedList.append(1);
 linkedList.append(2);
 linkedList.prepend(0);
-console.log(linkedList);
-console.log(linkedList.head);
-console.log(linkedList.tail);
+console.log(linkedList); // { value: 0, next: {value: 1, next: {value: 2, next: null } } }
+console.log(linkedList.head); // same as above
+console.log(linkedList.tail); // { value: 2, next: null }
+linkedList.pop();
+console.log(linkedList.size); // 2 because we removed the tail
+console.log(linkedList.at(2)); // null because we removed the node at index 2
+console.log(linkedList.contains(1)); // true
+console.log(linkedList.contains(999)); // false
+console.log(linkedList.find(56)); // null, inexistent value
+console.log(linkedList.find(1)); // node value 1 is at index 1
+console.log(linkedList.toString()); // (0) -> (1) -> null
